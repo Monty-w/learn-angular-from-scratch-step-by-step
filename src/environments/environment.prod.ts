fc818cf53419bf07d3271fecccab8ed85f98849b
Monty-w <mwoolam@gmail.com>
2024-04-13 22:00:51 -0500

    commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   //api_url: 'https://q-a-example-loopback-api.herokuapp.com'
-  api_url: 'http://snhumontybucket.s3-website-us-east-1.amazonaws.com/'
+  api_url: 'https://http://snhumontybucket.s3-website-us-east-1.amazonaws.com.us-east-1.amazonaws.com'
 };
