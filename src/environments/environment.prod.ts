597440cc6eb74f097fe3bfc21d12c156080f8b1e
Monty-w <mwoolam@gmail.com>
2024-04-25 21:43:30 -0500

    updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  //api_url: 'https://q-a-example-loopback-api.herokuapp.com'
-  api_url: 'https://gg6fjmo40m.execute-api.us-east-1.amazonaws.com'
+  api_url: 'https://q-a-example-loopback-api.herokuapp.com'
+  //api_url: 'https://gg6fjmo40m.execute-api.us-east-1.amazonaws.com'
 };
